a9272d964da79f7dc1b3109e1a3803335cd4c610
SruthiBijuGIT <128403226+SruthiBijuGIT@users.noreply.github.com>
2023-07-01 09:55:47 -0700

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int arr[100];
+    int N, i;
+    int * ptr = arr;  
+    printf("Enter size of an array: ");
+    scanf("%d", &N);
+    printf("Enter elements in an array:\n");
+    for (i = 0; i < N; i++)
+    {
+        scanf("%d", ptr);
+        ptr++;   
+    }
+    ptr = arr;
+
+    printf("Array elements: ");
+    for (i = 0; i < N; i++)
+    {
+        printf("%d, ", *ptr);
+        ptr++;
+    }
+
+    return 0;
+}
